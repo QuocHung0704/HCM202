1b88bcb9ef2643349a61f1c5586cc57fb9e753a8
Hưng Trương <hungtqse182075@fpt.edu.vn>
2026-01-12 22:22:13 +0700

    config cors

---
+++ src/main/java/org/quochung/hcm202/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package org.quochung.hcm202.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+//                         .allowedOrigins("https://ten-mien-cua-ban.vercel.app") // Sau này hãy thay bằng URL thật của Frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
